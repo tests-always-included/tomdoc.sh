ab2f0d84a6af1bf0dbc61377ff428d77a0d88856
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-02-08 01:20:58 +0100

    Add tomdoc.sh

---
+++ tomdoc.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Shell script for parsing TomDoc and generating pretty documentation from it
+#
+# Written by Mathias Lafeldt <mathias.lafeldt@gmail.com>
+#
+
+file="$1"
+
+cat "$file" |
+(
+    func=
+    doc=
+
+    while read -r line; do
+        case "$line" in
+        '# Public: '*)
+            doc="$doc$line
+"
+            ;;
+        '#'*)
+            test -n "$doc" && doc="$doc$line
+"
+            ;;
+        *'()'*)
+            test -n "$doc" && func="$(expr "$line" : '\(.*\)()')"
+            ;;
+        esac
+
+        test -n "$func" -a -n "$doc" && {
+            echo "- $func"
+            echo
+            echo "$doc" | sed -E 's/^#/ /'
+            echo
+            func=
+            doc=
+        }
+    done
+)

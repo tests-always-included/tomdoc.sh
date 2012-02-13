bb80bfa93c6aae9dce591edf1e6419f75a170560
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-02-13 22:01:57 +0100

    Save test results to different files

--- test/t0001-generate.sh
+++ test/t0001-generate.sh
@@ -7,13 +7,13 @@ test_description="Generate documentation"
 FIXTURES="$TEST_DIRECTORY/fixtures"
 
 test_expect_success "Generate plain text documentation" "
-    tomdoc.sh --text '$BUILD_DIR/tomdoc.sh' >result &&
-    test_cmp '$FIXTURES/tomdoc.sh.txt' result
+    tomdoc.sh --text '$BUILD_DIR/tomdoc.sh' >result.txt &&
+    test_cmp '$FIXTURES/tomdoc.sh.txt' result.txt
 "
 
 test_expect_success "Generate markdown documentation" "
-    tomdoc.sh --markdown '$BUILD_DIR/tomdoc.sh' >result &&
-    test_cmp '$FIXTURES/tomdoc.sh.md' result
+    tomdoc.sh --markdown '$BUILD_DIR/tomdoc.sh' >result.md &&
+    test_cmp '$FIXTURES/tomdoc.sh.md' result.md
 "
 
 test_done
